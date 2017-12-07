9bf227b91999ab43ce332d2a44a9ea66446c913c
baharevmv <31476163+baharevmv@users.noreply.github.com>
2017-12-08 01:06:15 +0300

    Made New Project. Add TabBarController, NavigationController and Custom View Controller for adding New Task to my ToDoList Создал новый проект. Добавил TabBarController, NavigationController и Custom View Controller для добавления новых задач в мой ToDoList

---
+++ ToDoList/ToDOList/ToDOList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDOList
+//
+//  Created by max on 07.12.17.
+//  Copyright © 2017 Maxxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDOList/ToDOList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDOList
+//
+//  Created by max on 07.12.17.
+//  Copyright © 2017 Maxxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
